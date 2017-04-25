7b2ca3d342328e2dbb13a76a95ac780f07ce6768
jameswalshmicrofocus <james.walsh@microfocus.com>
2017-04-25 15:34:20 +0100

    initial commit

---
+++ Python Challenge 2.py
@@ -0,0 +1,15 @@
+string = "http//www.pythonchallenge.com/pc/def/map.html"
+alphabet = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz , ,.'.'()()/ / / /"
+#string = string.replace(" ", "")
+decrypted_message = []
+to_list = ([str(i) for i in str(string)])
+to_alphabet = ([str(i) for i in str(alphabet)])
+
+for characters in string:
+        firstvar = to_alphabet.index(characters)
+        firstvar = firstvar + 2
+        decrypted_message.append(alphabet[firstvar])
+
+
+makeitastring = ''.join(map(str, decrypted_message))
+print(makeitastring)
