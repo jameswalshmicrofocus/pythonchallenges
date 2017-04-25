82ca29d7ebf7f3b9dd7eebef964fc253207489cc
james.walsh@microfocus.com <Surrey@26>
2017-04-25 20:20:35 +0100

    Reads all comments in file

--- Testing.py
+++ Testing.py
@@ -1,7 +1,17 @@
-filename = "90052.txt"
-path = "C:\\Users\\James\\Downloads\\channel"+filename
+from os import listdir
+from os.path import isfile, join
+mypath = 'C:\\Users\\James\\Downloads\\channel\\'
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+count = 0
+for file in onlyfiles:
+    filename = file
+    path = mypath + filename
+    with open(path, 'r') as myfile:
+        data = myfile.read().replace('\n', '')
+        print(data)
+        data = data.replace("Next nothing is ", "")
+        filename = data
+        filename = filename + ".txt"
+        count = count + 1
 
-
-print(path)
-    # with open('enable1.txt', 'r') as myfile:
-    #     data = myfile.read().replace('\n', '')
+print(count)
