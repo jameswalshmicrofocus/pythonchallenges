da668b5141fc0140febfeb4b98e93b4245979766
jameswalshmicrofocus <james.walsh@microfocus.com>
2017-04-28 11:30:25 +0100

    Reads zip file comments

--- Python Challenge 6.py
+++ Python Challenge 6.py
@@ -1,11 +1,23 @@
+import zipfile
 i = 100
+list=[]
 filename = "90052.txt"
+z = zipfile.ZipFile("C:\\Users\\jamesw\\Downloads\\channel.zip")
 while i > 8:
 
-    path = "C:\\Users\\James\\Downloads\\channel\\"+filename
-    with open(path, 'r') as myfile:
-        data = myfile.read().replace('\n', '')
-        print(data)
-        data = data.replace("Next nothing is ", "")
-        filename = data
-        filename = filename+".txt"
+    path = "C:\\Users\\jamesw\\Downloads\\channel\\"+filename
+    try:
+        with open(path, 'r') as myfile:
+            data = myfile.read().replace('\n', '')
+            # print(data)
+            data = data.replace("Next nothing is ", "")
+            filename = data
+            filename = filename+".txt"
+            list.append(z.getinfo(filename).comment)
+    except:
+        break
+
+
+for list in list:
+    print(list)
+
--- Testing.py
+++ Testing.py
@@ -1,17 +1,8 @@
-from os import listdir
-from os.path import isfile, join
-mypath = 'C:\\Users\\James\\Downloads\\channel\\'
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-count = 0
-for file in onlyfiles:
-    filename = file
-    path = mypath + filename
-    with open(path, 'r') as myfile:
-        data = myfile.read().replace('\n', '')
-        print(data)
-        data = data.replace("Next nothing is ", "")
-        filename = data
-        filename = filename + ".txt"
-        count = count + 1
+import zipfile
+filename = "90052.txt"
+data=[]
+z = zipfile.ZipFile("C:\\Users\\jamesw\\Downloads\\channel.zip")
 
-print(count)
+data.append(z.getinfo(filename).comment)
+
+print(data)
