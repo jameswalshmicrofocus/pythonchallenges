9b22ab1cea44b8474f6f06b8793e62502a849d03
jameswalshmicrofocus <james.walsh@microfocus.com>
2017-04-28 14:26:01 +0100

    Solution for challenge 7

--- Python Challenge 7.py
+++ Python Challenge 7.py
@@ -1,4 +1,14 @@
 from PIL import Image
+import re
+target_image = Image.open('oxygen.png')
 
-target_image = Image.open('oxygen.png').convert('LA')
-target_image.save('greyscale.png')
+print(target_image.getpixel((0,0)))
+
+row = [target_image.getpixel((x, target_image.height / 2)) for x in range(target_image.width)]
+print(row[::7])
+ords = [r for r, g, b, a in row if r == g == b]
+
+"".join(map(chr, ords))
+nums = re.findall("\d+", "".join(map(chr, ords)))
+print(nums)
+print("".join(map(chr, map(int, nums))))
