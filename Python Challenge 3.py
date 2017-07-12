dc6c35da5ab66354aab1cbd42a9077352f0f7034
jameswalshmicrofocus <james.walsh@microfocus.com>
2017-07-12 14:23:58 +0100

    Update Python Challenge 3.py
    sad

--- Python Challenge 3.py
+++ Python Challenge 3.py
@@ -3,3 +3,5 @@ with open('enable1.txt', 'r') as myfile:
     data=myfile.read().replace('\n', '')
 for match in re.findall('[a-z]{1}[A-Z]{3}[a-z]{1}[A-Z]{3}[a-z]{1}',data, re.DOTALL):
     print (match)
+    
+    print("Hello There")
